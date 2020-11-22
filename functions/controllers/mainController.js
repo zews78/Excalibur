c4d4527cd6f45e1455ba25fae6eb4ff678a61680
zews78 <shariquealam52@gmail.com>
2020-11-23 01:36:28 +0530

    firestore integrated

--- functions/controllers/mainController.js
+++ functions/controllers/mainController.js
@@ -1,12 +1,54 @@
-// const firebase = require('../firebase');
+const firebase = require('../firebase');
 // const isAuth = require('../utils/isAuth');
 // const keywordGenerator = require('../utils/keywordGenerator');
 
 
 exports.getHome = async(req, res) => {
 	// const auth = (await isAuth(req))[0];
-	res.render('main/home', {
-		pageTitle: 'Home',
-		// auth
-	});
+	try {
+		// const auth = (await isAuth(req))[0];
+
+		var Cntr = [];
+		const CntrRef = firebase.firestore()
+			.collection('centres')
+		const snapshot = await CntrRef.get();
+		snapshot.forEach(doc => {
+			Cntr.push({
+				id: doc.id,
+				...doc.data()
+			});
+		});
+		console.log(Cntr);
+		res.render('main/home.ejs', {
+			// auth,
+			pageTitle: 'Home',
+			Cntr
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.getHelp = async(req, res) => {
+	try {
+		const auth = (await isAuth(req))[0];
+
+		var Cntr = [];
+		const CntrRef = firebase.firestore()
+			.collection('centres')
+		const snapshot = await CntrRef.get();
+		snapshot.forEach(doc => {
+			Cntr.push({
+				id: doc.id,
+				...doc.data()
+			});
+		});
+		console.log(Cntr);
+		res.render('main/home.ejs', {
+			pageTitle: 'Home',
+			Cntr
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
