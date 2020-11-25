2b5e5b153e3b214c5e90b91e1a04aa91535073d3
zews78 <shariquealam52@gmail.com>
2020-11-25 21:07:21 +0530

    profile section partially completed

--- functions/routes/mainRouter.js
+++ functions/routes/mainRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mainController = require('../controllers/mainController');
-// const isAuth = require('../middlewares/isAuth');
+const isAuth = require('../middlewares/isAuth');
 // const redirectTo = require('../middlewares/redirectTo');
 
 const router = express.Router();
 router.get('/', mainController.getHome);
 router.get('/center', mainController.getCenter);
-router.get('/center/:centerId', mainController.getOneCenter);
+router.get('/center/:centerId', isAuth, mainController.getOneCenter);
 
 
 
