b6c39bc31fbcd6f83bfd9c3281c2ed98dd64c86d
zews78 <shariquealam52@gmail.com>
2021-01-15 17:02:31 +0530

    improoved user profile, added qr code and other details

--- functions/controllers/userController.js
+++ functions/controllers/userController.js
@@ -1,4 +1,5 @@
 const firebase = require('../firebase');
+const QRCode = require('qrcode');
 
 const isAuth = require('../utils/isAuth');
 
@@ -27,16 +28,31 @@ exports.getUserProfile = async (req, res) => {
       .limit(3)
       .get();
     let req_ticket = [];
+    let my_centres = [];
     if (!reqTicket.empty) {
       reqTicket.forEach(ticket => {
         let ticketData = ticket.data();
-        ticketData.id = ticket.id;
+        my_centres.push(ticketData.centre_uid);
         req_ticket.push(ticketData);
+
+        // ticketData.id = []        
+        QRCode.toDataURL(ticket.id, function(err, url) {
+          //'url' stores the url of the generated qr code
+          ticketData.qrcode = url;
+        // qr_code.push(url, "hello");
+        });
       });
     }
-
+    myCntr = [];
+    const promises = my_centres.map(u => firebase.firestore().collection("centres").doc(u).get());
+    var results = await Promise.all(promises);
+    results.map(docSnapshot => {
+      myCntr.push(docSnapshot.data().centre_name);
+      // console.log(docSnapshot.data(), "shiit");
+    });
     // console.log(req_ticket);
-
+    // console.log(my_centres);
+    // console.log(myCntr)
     // const productsSnapshot = await firebase.firestore()
     // 	.collection('products')
     // 	.where('uid', '==', userId)
@@ -65,10 +81,28 @@ exports.getUserProfile = async (req, res) => {
     // 	}
     // }
 
+    // below is the code to generate a qr code
+
+    // qr_code = [];
+    // req_ticket.forEach(ticket => {
+    //   console.log(ticket.qrcode);
+    // })
+    //    QRCode.toDataURL(req_ticket[i].id, function(err, url) {
+    //     //'url' stores the url of the generated qr code
+    //     qr_code.push(url, "hello");
+    //   })
+    //   // console.log(i);
+    //   // console.log(req_ticket[i].id);
+
+    // }
+    // console.log(qr_code);
+
+
 
     res.render('users/profile.ejs', {
       pageTitle: 'Profile',
       auth: true,
+      myCntr,
       req_ticket,
       // authorized: userId === req.uid,
       user: {
