b3032e84a0b4849d4b0b749bfb12c8bb3bb888e6
zews78 <shariquealam52@gmail.com>
2024-05-04 16:21:53 +0530

    made changes in file upload

--- functions/firebase.js
+++ functions/firebase.js
@@ -5,7 +5,8 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://excellerentum.firebaseio.com'
+  databaseURL: 'https://excellerentum.firebaseio.com',
+  storageBucket: "gs://excellerentum.appspot.com"
 });
 
 module.exports = admin;
