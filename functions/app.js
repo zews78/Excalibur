9a5432544d10851573d58e2a604a75d1e904ec7e
zews78 <shariquealam52@gmail.com>
2024-06-30 22:23:03 +0530

    first design implementation containing footer

--- functions/app.js
+++ functions/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 // Setting public path
-app.use(express.static('public'), express.static('public/css'));
+app.use(express.static('public'), express.static('public/css'), express.static('public/images'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
